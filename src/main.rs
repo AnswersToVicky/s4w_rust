c0a67ea52fe3fc6f2696b57b5d868502da42eae0
Viktor Södergren <viksod@gmail.com>
2022-09-29 05:32:15 +0200

    add crates image and minifb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate image;
+extern crate minifb;
+
+use std::env;
+use std::path::Path;
+
+use image::{GenericImageView};
+
 fn main() {
-  println!("Hello, world!");
+    let file = if env::args().count() == 2 {
+        env::args().nth(1).unwrap()
+    } else {
+        panic!("Please enter a file path as argument.")
+    };
+
+    // Use the open function to load an image from a Path.
+    // ```open``` returns a dynamic image.
+    let im = image::open(&Path::new(&file)).unwrap();
+
+    // The dimensions method returns the images width and height
+    println!("dimensions {:?}", im.dimensions());
+
+    // The color method returns the image's ColorType
+    println!("{:?}", im.color());
 }
